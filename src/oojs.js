13365db1352c1a8840a3e1437e9a76d301f483cd
Vikas Burman <vikasburman@gmail.com>
2017-09-10 16:33:06 +0530

    fixed

--- src/oojs.js
+++ src/oojs.js
@@ -1178,19 +1178,18 @@
                 // static ones are defined just as is, e.g.,
                 //  ('text', 012, false, Reference)
                 // dynamic ones are defined as special string
-                //  ('[funcOrPropName]', 012, false, Reference)
+                //  ('[publicPropOrFuncName]', 012, false, Reference)
                 // when string is defined as '[...]', this argument is replaced by a 
                 // function which can be called (with binded this) to get dynamic value of the argument
-                // the funcOrPropName is the name of a private/protected/public
-                // function/property of the same object where this attribute is applied
+                // the publicPropName is the name of a public property or function
+                // name of the same object where this attribute is applied
                 this.args = [];
                 for(let arg of args) {
                     if (typeof arg === 'string') {
                         if (arg.startsWith('[') && arg.endsWith(']')) {
                             let fnName = arg.replace('[', '').replace(']', ''),
                                 fn = function() {
-                                    let obj = as(this, 'protected'),
-                                        member = obj[fnName];
+                                    let member = this[fnName]; // this would change because of binding call when this function is called
                                     if (typeof member === 'function') {
                                         return member();
                                     } else {
