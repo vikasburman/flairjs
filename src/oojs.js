cdda3dff96247ae4f05d78e35572225ca68e1566
Vikas Burman <vikasburman@gmail.com>
2017-09-16 17:00:05 +0530

    fixed

--- src/oojs.js
+++ src/oojs.js
@@ -1174,7 +1174,7 @@
         oojs.Attribute = oojs.Class('Attribute', function() {
             let decoratorFn = null;
             this.func('constructor', (...args) => {
-                // args can be static or dynamic
+                // args can be static or dynamic or settings
                 // static ones are defined just as is, e.g.,
                 //  ('text', 012, false, Reference)
                 // dynamic ones are defined as special string
@@ -1183,13 +1183,14 @@
                 // function which can be called (with binded this) to get dynamic value of the argument
                 // the publicPropName is the name of a public property or function
                 // name of the same object where this attribute is applied
+                // settings ones are defined as another special string
                 this.args = [];
                 for(let arg of args) {
                     if (typeof arg === 'string') {
                         if (arg.startsWith('[') && arg.endsWith(']')) {
                             let fnName = arg.replace('[', '').replace(']', ''),
                                 fn = function() {
-                                    let member = this[fnName]; // this would change because of binding call when this function is called
+                                    let member = this[fnName]; // 'this' would change because of binding call when this function is called
                                     if (typeof member === 'function') {
                                         return member();
                                     } else {
