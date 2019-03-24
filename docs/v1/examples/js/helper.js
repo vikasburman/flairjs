b7307e94ecec0a76ed1b10c4c1bdd79e4d6df679
Vikas Burman <vikasburman@gmail.com>
2019-03-24 17:07:22 -0400

    ...

--- docs/v1/examples/js/helper.js
+++ docs/v1/examples/js/helper.js
@@ -20,3 +20,25 @@ new Vue({
         log: viewlog
     }
 });
+
+// include keywords
+const { Class, Struct, Enum, Interface, Mixin } = flair;
+const { Aspects } = flair;
+const { AppDomain } = flair;
+const __currentContextName = flair.AppDomain.context.current().name;
+const { $$, attr } = flair;
+const { bring, Container, include } = flair;
+const { Port } = flair;
+const { on, post, telemetry } = flair;
+const { Reflector } = flair;
+const { Serializer } = flair;
+const { Tasks } = flair;
+const { TaskInfo } = flair.Tasks;
+const { as, is, isComplies, isDerivedFrom, isImplements, isInstanceOf, isMixed } = flair;
+const { getAssembly, getAttr, getContext, getResource, getRoute, getType, ns, getTypeOf, typeOf } = flair;
+const { dispose, using } = flair;
+const { Args, Exception, noop, nip, nim, nie, event } = flair;
+const { env } = flair.options;
+const { forEachAsync, replaceAll, splitAndTrim, findIndexByProp, findItemByProp, which, isArrowFunc, isASyncFunc, sieve, b64EncodeUnicode, b64DecodeUnicode } = flair.utils;
+const { $static, $abstract, $virtual, $override, $sealed, $private, $privateSet, $protected, $protectedSet, $readonly, $async } = $$;
+const { $overload, $enumerate, $dispose, $post, $on, $timer, $type, $args, $inject, $resource, $asset, $singleton, $serialize, $deprecate, $session, $state, $conditional, $noserialize, $ns } = $$;
