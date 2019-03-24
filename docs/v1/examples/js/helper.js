14324049c62ae045907745cec472e5c479bc2f4e
Vikas Burman <vikasburman@gmail.com>
2019-03-24 16:55:25 -0400

    ...

--- docs/v1/examples/js/helper.js
+++ docs/v1/examples/js/helper.js
@@ -9,7 +9,7 @@ let viewlog = [];
 
 // logger function
 const logger = function(msg, addBreak) {
-  if (addBreak) { viewlog.push('\n'); viewlog.push('\n'); viewlog.push('\n'); }
+  if (addBreak) { viewlog.push('-'); }
 	viewlog.push(msg);
 }
 
