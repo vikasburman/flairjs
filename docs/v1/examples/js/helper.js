3940ee53204965b06a9b4535e76bad4c1d673d3a
Vikas Burman <vikasburman@gmail.com>
2019-03-24 17:25:17 -0400

    ...

--- docs/v1/examples/js/helper.js
+++ docs/v1/examples/js/helper.js
@@ -1,7 +1,7 @@
 // define logging ui
 let el = document.createElement('div');
 el.id = 'demo';
-el.innerHTML = '<ul style="list-style: none;"><li v-for="line in log">{{ line }}</li></ul>';
+el.innerHTML = '<ul style="margin: 0; list-style: none;"><li v-for="line in log">{{ line }}</li></ul>';
 document.firstElementChild.append(el)
 
 // define logging bucket
