ae3939c0abbfe839b10b987e5cff83bd0b209a46
Vikas Burman <vikasburman@VikasBurmansMBP.fios-router.home>
2019-05-04 23:03:07 -0400

    ...

--- dist/flair.cli.js
+++ dist/flair.cli.js
@@ -5,8 +5,8 @@
  * 
  * Assembly: flair.cli
  *     File: ./flair.cli.js
- *  Version: 0.50.47
- *  Sun, 05 May 2019 03:02:06 GMT
+ *  Version: 0.50.48
+ *  Sun, 05 May 2019 03:02:56 GMT
  * 
  * (c) 2017-2019 Vikas Burman
  * Licensed under MIT
@@ -23,7 +23,7 @@ const fsx = require('fs-extra');
 const del = require('del');
 const buildInfo = {
     name: 'flair.cli',
-    version: '0.50.47',
+    version: '0.50.48',
     format: 'fasm',
     formatVersion: '1',
     contains: [
