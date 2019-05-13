5a5aed54811ae1c581afee91e7f7013cb05ba89a
Vikas Burman <vikasburman@VikasBurmansMBP.fios-router.home>
2019-05-13 15:15:55 -0400

    ...

--- docs/v1/examples/js/flair.js
+++ docs/v1/examples/js/flair.js
@@ -5,8 +5,8 @@
  * 
  * Assembly: flair
  *     File: ./flair.js
- *  Version: 0.6.81
- *  Mon, 13 May 2019 19:14:03 GMT
+ *  Version: 0.6.82
+ *  Mon, 13 May 2019 19:15:28 GMT
  * 
  * (c) 2017-2019 Vikas Burman
  * MIT
@@ -7022,10 +7022,10 @@
         name: 'flairjs',
         title: 'Flair.js',
         file: currentFile,
-        version: '0.6.81',
+        version: '0.6.82',
         copyright: '(c) 2017-2019 Vikas Burman',
         license: 'MIT',
-        lupdate: new Date('Mon, 13 May 2019 19:14:03 GMT')
+        lupdate: new Date('Mon, 13 May 2019 19:15:28 GMT')
     });  
 
     // bundled assembly load process 
@@ -7422,7 +7422,7 @@
         AppDomain.context.current().currentAssemblyBeingLoaded('');
         
         // register assembly definition object
-        AppDomain.registerAdo('{"name":"flair","file":"./flair{.min}.js","mainAssembly":"flair","desc":"True Object Oriented JavaScript","title":"Flair.js","version":"0.6.81","lupdate":"Mon, 13 May 2019 19:14:03 GMT","builder":{"name":"flairBuild","version":"1","format":"fasm","formatVersion":"1","contains":["init","func","type","vars","reso","asst","rout","sreg"]},"copyright":"(c) 2017-2019 Vikas Burman","license":"MIT","types":["Aspect","Attribute","IDisposable","IProgressReporter","Task"],"resources":[],"assets":[],"routes":[]}');
+        AppDomain.registerAdo('{"name":"flair","file":"./flair{.min}.js","mainAssembly":"flair","desc":"True Object Oriented JavaScript","title":"Flair.js","version":"0.6.82","lupdate":"Mon, 13 May 2019 19:15:28 GMT","builder":{"name":"flairBuild","version":"1","format":"fasm","formatVersion":"1","contains":["init","func","type","vars","reso","asst","rout","sreg"]},"copyright":"(c) 2017-2019 Vikas Burman","license":"MIT","types":["Aspect","Attribute","IDisposable","IProgressReporter","Task"],"resources":[],"assets":[],"routes":[]}');
         
         // assembly load complete
         if (typeof onLoadComplete === 'function') { 
